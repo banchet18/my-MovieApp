e33d5796ffb139aec2c20ed73d3b3c25b2bf1cb0
banchet18 <banjarac6@gmail.com>
2024-02-05 12:22:23 +0530

    c

--- my-movie-app/src/Route/index.js
+++ my-movie-app/src/Route/index.js
@@ -1,4 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomeContainer from "../Container/Home";
+
 const RouterComponent = () => {
-  return <div>RouterComponent</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomeContainer />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 export default RouterComponent;
